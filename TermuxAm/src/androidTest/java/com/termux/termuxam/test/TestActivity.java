00393377ba280f7dff04633019105cea68994864
heworkalin <2701814400@qq.com>
2026-04-13 22:47:45 +0800

    ...

---
+++ TermuxAm/src/androidTest/java/com/termux/termuxam/test/TestActivity.java
@@ -0,0 +1,20 @@
+package com.termux.termuxam.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.termux.termuxam.IActivityManagerTest;
+
+/**
+ * {@link Activity} used for {@link IActivityManagerTest#testStartActivity()}
+ */
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TestComponentsService.noteEvent("TestActivity " + getIntent().getAction());
+        finish();
+    }
+}
